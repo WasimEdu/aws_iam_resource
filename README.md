df777802ce3a41ef2ebe1db018e85453a4ae2994
WasimEdu <WASIM.20231CSG0054@presidencyuniversity.in>
2024-08-29 00:19:07 +0530

    Updated README.md

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 created by: Wasim
 <br>
-A python code / script to list
+A python code / script to list all present users in AWS IAM
+<h3>
+Installion
+<br>
+Install "boto3" module before using the code
+<br>
+"pip3 install boto3"
+<br>
+AWS CLI should be Configure
--- iam_users_aws.py
+++ iam_users_aws.py
@@ -1,8 +1,12 @@
+# importing modules
 import boto3
 
+# opening aws managment console
 aws_console = boto3.session.Session(profile_name = 'default')
 
+# opening iam console as resource
 iam_console = aws_console.resource('iam')
 
+# printing list of users in iam
 for users in iam_console.users.all():
     print(users.name)
